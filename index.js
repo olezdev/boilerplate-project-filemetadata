d9c994657aaf18f94f5eaf629446e80839c5a47b
olezdev <olezdev@gmail.com>
2023-12-19 17:52:26 -0300

    File Metadata Microservice

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'Not file uploaded' });
+  }
 
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  };
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  res.json(fileInfo);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Your app is listening on port ${PORT}`)
 });
